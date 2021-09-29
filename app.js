434283feee2d6fb1d3401a068ba893760dfeca20
tizuemg <tizuemg@gmail.com>
2021-09-30 01:18:09 +0300

    Second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Tizu!'));
+app.get('/', (req, res) => res.send('Hello World! This is Tizu! Updated!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
